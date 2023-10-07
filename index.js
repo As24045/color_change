6188b551f87aafe4d433b3d49b1c3fb2645e2a4c
Amit Kumar Sharma <138234619+As24045@users.noreply.github.com>
2023-10-07 13:31:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click', function(event) {
+        console.log(event);
+        console.log(event.target);
+
+    if (event.target.id === "grey") {
+    body.style.backgroundColor = event.target.id;
+    }
+
+    if (event.target.id === "white") {
+        body.style.backgroundColor = event.target.id;
+        };
+
+        if (event.target.id === "green") {
+            body.style.backgroundColor = event.target.id;
+            };
+
+            if (event.target.id === "red") {
+                body.style.backgroundColor = event.target.id;
+                };
+
+                    
+            
+    });
+});
